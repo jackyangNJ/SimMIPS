0ef0d2efdb5b1a19e5ed2ea677fd390c35bab5b1
jackyang <467287824@qq.com>
2015-03-03 10:16:54 +0800

    fix gpio driver bug

--- sw/BootRom/src/common/drivers/gpio.c
+++ sw/BootRom/src/common/drivers/gpio.c
@@ -2,41 +2,49 @@
 #include "drivers/gpio.h"
 
 #define GPIO_REG_BASE 0xB8000400
-#define GPIO_REG_CTRL 0xB8000404
-#define GPIO_REG_DATA 0xB8000400
 
 void gpio_set_out(int num)
 {
-    int data = in_long(GPIO_REG_CTRL);
-    data |= (1 << num);
-    out_long(GPIO_REG_CTRL, data);
+    int addr = num/16*4 + GPIO_REG_BASE;
+    int offset = num % 16;
+    int data = in_long(addr);
+    data |= (1 << (offset*2));
+    out_long(addr, data);
 }
 void gpio_set_in(int num)
 {
-    int data = in_long(GPIO_REG_CTRL);
-    data &= ~(1 << num);
-    out_long(GPIO_REG_CTRL, data);
+    int addr = num/16*4 + GPIO_REG_BASE;
+    int offset = num % 16;
+    int data = in_long(addr);
+    data &= ~(1 << (offset*2));
+    out_long(addr, data);
 
 }
 
 void gpio_set(int num)
 {
-    int data = in_long(GPIO_REG_DATA);
-    data |= (1 << num);
-    out_long(GPIO_REG_DATA, data);
+    int addr = num/16*4 + GPIO_REG_BASE;
+    int offset = num % 16;
+    int data = in_long(addr);
+    data |= (1 << (offset*2+1));
+    out_long(addr, data);
 
 }
  
 void gpio_clear(int num)
 {
-    int data = in_long(GPIO_REG_DATA);
-    data &= ~(1 << num);
-    out_long(GPIO_REG_DATA, data);
+    int addr = num/16*4 + GPIO_REG_BASE;
+    int offset = num % 16;
+    int data = in_long(addr);
+    data &= ~(1 << (offset*2+1));
+    out_long(addr, data);
 }
  
 int gpio_get(int num)
 {
-    int data = in_long(GPIO_REG_DATA);
-    data &= (1 << num);
+    int addr = num/16*4 + GPIO_REG_BASE;
+    int offset = num % 16;
+    int data = in_long(addr);
+    data &= (1 << (offset*2+1));
     return (data != 0);
 }
