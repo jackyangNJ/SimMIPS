7c56a8b271e9e6a06afd1670d20b01cfc1be2620
jackyang <467287824@qq.com>
2015-03-06 08:53:25 +0800

    modify bard divisor to satisfy the new clock frequency 100M

--- sw/BootRom/src/common/drivers/serial.c
+++ sw/BootRom/src/common/drivers/serial.c
@@ -2,80 +2,74 @@
 #include "mips/io.h"
 #include "vfprintf.h"
 
-void serial_init(void) 
-{
-	out_byte(SERIAL_PORT + 1, 0x00);
-	out_byte(SERIAL_PORT + 3, 0x80);
-	out_byte(SERIAL_PORT + 0, 27);
-	out_byte(SERIAL_PORT + 1, 0x00);
-	out_byte(SERIAL_PORT + 3, 0x03);
-	out_byte(SERIAL_PORT + 2, 0xC7);
-	out_byte(SERIAL_PORT + 4, 0x0B);
-	//enable receive intterrupt
-	// out_byte(SERIAL_PORT + 1, 0x01);
+void serial_init(void) {
+    //disable the uart device
+    out_byte(SERIAL_PORT + 1, 0x00);
+    //get access to divisor latches
+    out_byte(SERIAL_PORT + 3, 0x80);
+    //write value to divisor latches to determine the bard rate
+    //for 100MHz clock, current uart baud is 115200.
+    //Divisor = 100M /16 / 115200;
+    out_byte(SERIAL_PORT + 0, 54);
+    out_byte(SERIAL_PORT + 1, 0x00);
+    //config  8-bit mode
+    out_byte(SERIAL_PORT + 3, 0x03);
+    //clear fifo and set the fifo trigger level
+    out_byte(SERIAL_PORT + 2, 0xC7);
+    out_byte(SERIAL_PORT + 4, 0x0B);
+    //enable receive intterrupt
+    // out_byte(SERIAL_PORT + 1, 0x01);
 }
 
-
-
-inline int serial_idle(void) 
-{
-	return (in_byte(SERIAL_PORT + 5) & 0x20) != 0;
+inline int serial_idle(void) {
+    return (in_byte(SERIAL_PORT + 5) & 0x20) != 0;
 }
 
-static inline int serial_has_data()
-{
-	return (in_byte(SERIAL_PORT + 5) & 0x01) != 0;
+static inline int serial_has_data() {
+    return (in_byte(SERIAL_PORT + 5) & 0x01) != 0;
 }
 
-void serial_printc(char ch) 
-{
-	while (serial_idle() != 1);
-	out_byte(SERIAL_PORT, ch);
+void serial_printc(char ch) {
+    while (serial_idle() != 1);
+    out_byte(SERIAL_PORT, ch);
 }
 
-void serial_prints(const char* str)
-{
-    while(*str != '\0') { /* Loop until end of string */
-        serial_printc( *str );
+void serial_prints(const char* str) {
+    while (*str != '\0') { /* Loop until end of string */
+        serial_printc(*str);
         str++;
     }
 }
 
-int serial_printf(const char *ctl, ...)
-{
-	void **args = (void **)&ctl + 1;
-	return vfprintf(ctl, args, serial_printc);
+int serial_printf(const char *ctl, ...) {
+    void **args = (void **) &ctl + 1;
+    return vfprintf(ctl, args, serial_printc);
 }
-
 
 /**
  * in little endian
  */
-int serial_read_int()
-{
-	int rtn = 0;
-	int i;
-	for(i=0;i<4;i++){
-		uint8_t tmp = serial_read_byte();
-		rtn |= tmp << (8*i);
-	}
-	return rtn;
+int serial_read_int() {
+    int rtn = 0;
+    int i;
+    for (i = 0; i < 4; i++) {
+        uint8_t tmp = serial_read_byte();
+        rtn |= tmp << (8 * i);
+    }
+    return rtn;
 }
 
-uint8_t serial_read_byte()
-{
-	while(serial_has_data() == 0);
-	return in_byte(SERIAL_PORT);
+uint8_t serial_read_byte() {
+    while (serial_has_data() == 0);
+    return in_byte(SERIAL_PORT);
 }
 
-void serial_read(uint8_t* str,int length)
-{
-	int i;
-	for(i=0;i<length;i++)
-		str[i] = serial_read_byte();
+void serial_read(uint8_t* str, int length) {
+    int i;
+    for (i = 0; i < length; i++)
+        str[i] = serial_read_byte();
 }
 
-void serial_flush()
-{
-	while(serial_idle() != 1);
+void serial_flush() {
+    while (serial_idle() != 1);
 }
